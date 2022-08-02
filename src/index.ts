972845dbd467d90872c434f975bba4e406d0be84
Jan Rolenc <jrolenc@centrum.cz>
2022-08-02 16:21:15 +0200

    started implementing interface

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 /// <reference types="@types/google.maps" />
-// import { User } from './User';
-// import { Company } from './Company';
+import { User } from './User'
+import { Company } from './Company'
+import { CustomMap } from './CustomMap'
 
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-  zoom: 1,
-  center: { lat: 0, lng: 0 },
-})
+const customMap = new CustomMap('map')
+const user = new User()
+const company = new Company()
+console.log('user', user)
+console.log('company', company)
+// customMap.addUserMarker(user)
+// customMap.addCompanyMarker(company)
+customMap.addMarker(user)
+customMap.addMarker(company)
